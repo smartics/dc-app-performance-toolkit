cc6e27193ae8a4fbd49c8d325d044ef16414ae56
Anton Kronseder <anton.kronseder@smartics.de>
2022-08-02 10:10:13 +0200

    logging

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -16,18 +16,21 @@ from locustio.common_utils import init_logger, confluence_measure
 
 logger = init_logger(app_type='confluence')
 
-TESTCASE_SPACE_KEY = "TC"
+TESTCASE_SPACE_KEY = "DELAAATEST"
 TC_DISPLAY_TABLE = "Test+Case+Display+Table"
 TC_DISPLAY_TABLE_ASSERTION_TEXT = "List of Documents"
 TC_TRANSCLUDE_DOCUMENTS = "Test+Case+Transclude+from+Documents"
 TC_TRANSCLUDE_DOCUMENTS_ASSERTION_TEXT="Transclusion from Documents"
 
-@confluence_measure
+logger.error(f"XXXXXYYYY  LOCUST AAAAAAAAAAAAAAA")
+
+@confluence_measure("locust_app_specific_action")
 def app_specific_action(locust):
     """
     Calls the use case where multiple documents are listed in a Display Table
     Macro and Transclude Documents Macro.
     """
+    logger.error(f"XXXXXYYYY  LOCUST DISPLAYTABLE")
     response = locust.get(
         '/confluence/display/{}/{}'.format(TESTCASE_SPACE_KEY,
                                            TC_DISPLAY_TABLE),
@@ -38,3 +41,20 @@ def app_specific_action(locust):
     if assertion_string not in content:
         logger.error(f"'{assertion_string}' was not found in {content}")
     assert assertion_string in content
+
+
+"""
+@confluence_measure("locust_app_specific_action")
+def app_specific_action2(locust):
+    logger.error(f"XXXXX  LOCUST TRANSCLUSION")
+    response = locust.get(
+        '/confluence/display/{}/{}'.format(TESTCASE_SPACE_KEY,
+                                           TC_TRANSCLUDE_DOCUMENTS),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+
+    assertion_string = TC_TRANSCLUDE_DOCUMENTS_ASSERTION_TEXT
+    if assertion_string not in content:
+        logger.error(f"'{assertion_string}' was not found in {content}")
+    assert assertion_string in content
+"""
