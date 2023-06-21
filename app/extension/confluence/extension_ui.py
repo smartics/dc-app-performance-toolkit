b151a99994c134b01f612b58dfb6ead148b9ef21
Anton Kronseder <anton.kronseder@smartics.de>
2023-06-21 15:05:05 +0200

    2023-userscripts

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -7,6 +7,39 @@ from selenium_ui.conftest import print_timing
 from selenium_ui.confluence.pages.pages import Login, AllUpdates
 from util.conf import CONFLUENCE_SETTINGS
 
+def banner_is_appearing(webdriver, datasets):
+    page = BasePage(webdriver)
+    if datasets['custom_pages']:
+        app_specific_page_id = datasets['custom_page_id']
+
+    # To run action as specific user uncomment code bellow.
+    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
+    # just before test_2_selenium_z_log_out
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.wait_for_page_loaded()
+            login_page.set_credentials(username=username, password=password)
+            login_page.click_login_button()
+            if login_page.is_first_login():
+                login_page.first_user_setup()
+            all_updates_page = AllUpdates(webdriver)
+            all_updates_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
+
+    @print_timing("selenium_app_custom_action")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:view_page")
+        def sub_measure():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
+            page.wait_until_present((By.ID, "userscripts-dc-test-banner"))  # Wait for you app-specific UI element by ID selector
+        sub_measure()
+    measure()
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
@@ -16,21 +49,21 @@ def app_specific_action(webdriver, datasets):
     # To run action as specific user uncomment code bellow.
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
     # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.wait_for_page_loaded()
+            login_page.set_credentials(username=username, password=password)
+            login_page.click_login_button()
+            if login_page.is_first_login():
+                login_page.first_user_setup()
+            all_updates_page = AllUpdates(webdriver)
+            all_updates_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
@@ -39,6 +72,6 @@ def app_specific_action(webdriver, datasets):
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_present((By.ID, "userscripts-admin-tool"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
