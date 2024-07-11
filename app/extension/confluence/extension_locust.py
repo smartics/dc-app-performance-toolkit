9b62ce085009e47ece050894e00e665de486bc56
AntonKronseder <anton.kronseder@smartics.de>
2024-07-11 22:40:56 +0200

    pd

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -3,9 +3,49 @@ from locustio.common_utils import init_logger, confluence_measure, run_as_specif
 
 logger = init_logger(app_type='confluence')
 
+# smartics
+# smartics switch between userscripts and projectdoc toolbox
+#TESTCASE_SPACE_KEY = "DOCM"
+TESTCASE_SPACE_KEY = "PROJECTDOCTEST"
+
+#DocumentationMacros (And Toolbox ?)
+TC_DOCM = "DOCM1"
+TC_DOCM_SECTION_ASSERTION_TEXT = "Section1"
+TC_DOCM_HIDE_ASSERTION_TEXT = "HideFromAll"
+TC_DOCM_HIDEFROMANONYMOUS_ASSERTION_TEXT = "HideFromAnonymous"
+TC_DOCM_HIDEFROMREADER_ASSERTION_TEXT = "HideFromReader"
+TC_DOCM_DEFINITIONLIST_ASSERTION_TEXT = "Value2"
+
+#ProjectdocToolbox (TC = TestCase)
+TC_DISPLAY_TABLE = "Test Case Display Table"
+TC_DISPLAY_TABLE_ASSERTION_TEXT = "List of Documents"
+
+TC_TRANSCLUDE_DOCUMENTS = "Test Case Transclude from Documents"
+TC_TRANSCLUDE_DOCUMENTS_ASSERTION_TEXT = "Transclusion from Documents"
+
+#Extension Informationssystem
+TC_INFORMATIONSYSTEM_TEST = "Informationsystemtest"
+TC_INFORMATIONSYSTEM_ASSERTION_TEXT = "informationsystem-test-case-id"
+
+#smartics comment in only the wanted tests
+
+@confluence_measure("locust_app_specific_action_userscript_rest")
+def app_specific_action_userscript_rest(locust):
+    page_id = "44236807"
+    expected_text = "test-1.0.0.js"
+    url = f'/rest/userscripts-for-confluence/1/context?page-id={page_id}'
+    logger.info(f"Requesting Userscripts RestAPI(1) content for: PAGEID {page_id}")
+    with locust.client.get(url, catch_response=True) as response:
+        if response.status_code == 200:
+            content = response.text
+            if expected_text in content:
+                response.success()
+            else:
+                response.failure(f"Expected text '{expected_text}' not found in response content.")
+        else:
+            response.failure(f"Request failed with status code {response.status_code}")
 
 @confluence_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
     r = locust.get('/rest/projectdoc/1/document?select=Title%2CName%2CIteration&from=PROJECTDOCTEST&where=%24%3CTitle%3E%3D%5Bprojectdoc%20Space%20for%20Test%20Cases%5D&expand=property',
                    catch_response=True)  # call app-specific GET endpoint
@@ -18,3 +58,201 @@ def app_specific_action(locust):
     if token == "":
         logger.error(f"'assertion string' was not found in {content}")
     assert token != ""  # assert that TOKEN is not empty
+
+#@confluence_measure("locust_app_specific_action")
+#def app_specific_action(locust):
+#    logger.info(f"Userscripts RestAPI content 1")
+# http://a9ad5e96ce29c45b1a45a1d85829e3a3-1034839394.us-east-2.elb.amazonaws.com/confluence/rest/userscripts-for-confluence/1/context?page-id=44236807
+#    response = locust.get('/rest/userscripts-for-confluence/1/repo/de.smartics.test/test-1.0.0.js', catch_response=True)
+#    content = response.content.decode('utf-8')
+#    assert_text(content, "Copyright 2019-2020 Kronseder & Reiner GmbH, smartics")
+
+#@confluence_measure("locust_app_specific_action")
+#def app_specific_action(locust):
+#    logger.info(f"Atlassian RestAPI check")
+#    response = locust.get('/rest/api/user/current', catch_response=True)
+#    content = response.content.decode('utf-8')
+#    assert_text(content, "type")
+
+
+@confluence_measure("locust_app_specific_action_docm_section")
+def app_specific_action_docm_section(locust):
+    logger.info(f"SectionMacro DocumentationMacro")
+    response = locust.get('/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_DOCM), catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_DOCM_SECTION_ASSERTION_TEXT)
+
+@confluence_measure("locust_app_specific_action_docm_hide")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_docm_hide(locust):
+    logger.info(f"HideMacro DocumentationMacro")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_DOCM),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_DOCM_HIDE_ASSERTION_TEXT)
+
+
+@confluence_measure("locust_app_specific_action_docm_hidefromreader")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_docm_hidefromreader(locust):
+    logger.info(f"HideFromReaderMacro DocumentationMacro")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_DOCM),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_DOCM_HIDEFROMREADER_ASSERTION_TEXT)
+
+
+@confluence_measure("locust_app_specific_action_docm_hidefromanonymous")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_docm_hidefromanonymous(locust):
+    logger.info(f"HideFromAnonymousMacro DocumentationMacro")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_DOCM),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_DOCM_HIDEFROMANONYMOUS_ASSERTION_TEXT)
+
+
+@confluence_measure("locust_app_specific_action_docm_definitionlist")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_docm_definitionlist(locust):
+    logger.info(f"Definitionlist DocumentationMacro")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_DOCM),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_DOCM_DEFINITIONLIST_ASSERTION_TEXT)
+
+@confluence_measure("locust_app_specific_action_transclude_documents")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_transclude_documents(locust):
+    logger.info(f"TranscludeDocuments")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_TRANSCLUDE_DOCUMENTS),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_TRANSCLUDE_DOCUMENTS_ASSERTION_TEXT)
+
+@confluence_measure("locust_app_specific_action_display_table")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_display_table(locust):
+    logger.info(f"DisplayTable")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_DISPLAY_TABLE),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_DISPLAY_TABLE_ASSERTION_TEXT)
+
+@confluence_measure("locust_app_specific_action_information_system")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_information_system(locust):
+    logger.info(f"Informationsystems")
+    response = locust.get(
+        '/display/{}/{}'.format(TESTCASE_SPACE_KEY, TC_INFORMATIONSYSTEM_TEST),
+        catch_response=True)
+    content = response.content.decode('utf-8')
+    assert_text(content, TC_INFORMATIONSYSTEM_ASSERTION_TEXT)
+
+
+#REST API (Web API = wa)
+@confluence_measure("locust_app_specific_action_web_api")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+def app_specific_action_web_api(locust):
+    r = locust.get('/rest/projectdoc/1/document?select=Title%2CName%2CIteration&from=PROJECTDOCTEST&where=%24%3CTitle%3E%3D%5Bprojectdoc%20Space%20for%20Test%20Cases%5D&expand=property',
+                   catch_response=True)  # call app-specific GET endpoint
+    content = r.content.decode('utf-8')  # decode response content
+
+    token_pattern_example = '"id-list":"(.+?)"'
+    token = re.findall(token_pattern_example,
+                       content)  # get TOKEN from response using regexp
+    logger.locust_info(f'token: {token}')  # log info for debug when verbose is true in confluence.yml file
+    if token == "":
+        logger.error(f"'assertion string' was not found in {content}")
+    assert token != ""  # assert that TOKEN is not empty
+
+
+#@confluence_measure("locust_app_specific_action")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+#def app_specific_action(locust):
+#    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+#    content = r.content.decode('utf-8')   # decode response content
+
+#    token_pattern_example = '"token":"(.+?)"'
+#    id_pattern_examplem= '"id":"(.+?)"'
+#    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
+#    id = remfindall(id_pattern_example, content)    # get id from response using regexp
+
+#    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in confluence.yml file
+#    if 'assertion string' not in content:
+#        logger.error(f"'assertion string' was not found in {content}")
+#    assert 'assertion string' in content  # assert specific string in response content
+
+#    body = {"id": id, "token": token}  # include parsed variables to POST request body
+#    headers = {'content-type': 'application/json'}
+#    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
+#    content = r.content.decode('utf-8')
+#    if 'assertion string after successful POST request' not in content:
+#        logger.error(f"'assertion string after successful POST request' was not found in {content}")
+#    assert 'assertion string after successful POST request' in content  # assertion after POST request
+
+
+def assert_text(content, assertion_string):
+    if assertion_string not in content:
+        logger.error(f"'{assertion_string}' was not found in {content}")
+        assert assertion_string in content
+    else:
+        logger.info(
+            f"'{assertion_string}' was found in content")
+
+# {
+#     "size": 1,
+#     "start-index": 0,
+#     "max-result": 1,
+#     "id-list": "43647154",
+#     "document": [
+#         {
+#             "id": 43647154,
+#             "property": [
+#                 {
+#                     "name": "Title",
+#                     "value": "projectdoc Space for Test Cases",
+#                     "controls": "artificial, is-single-value"
+#                 },
+#                 {
+#                     "name": "Name",
+#                     "value": "\n                    projectdoc Space for Test Cases\n                    \n                  ",
+#                     "controls": "hide"
+#                 }
+#             ],
+#             "section": []
+#         }
+#     ]
+# }
+
+
+
+#@confluence_measure("locust_app_specific_action")
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+#def app_specific_action(locust):
+#    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+#    content = r.content.decode('utf-8')   # decode response content
+
+#    token_pattern_example = '"token":"(.+?)"'
+#    id_pattern_example = '"id":"(.+?)"'
+#    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
+#    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+
+#    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in confluence.yml file
+#    if 'assertion string' not in content:
+#        logger.error(f"'assertion string' was not found in {content}")
+#    assert 'assertion string' in content  # assert specific string in response content
+
+#    body = {"id": id, "token": token}  # include parsed variables to POST request body
+#    headers = {'content-type': 'application/json'}
+#    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
+#    content = r.content.decode('utf-8')
+#    if 'assertion string after successful POST request' not in content:
+#        logger.error(f"'assertion string after successful POST request' was not found in {content}")
+#    assert 'assertion string after successful POST request' in content  # assertion after POST request
--- app/locustio/confluence/locustfile.py
+++ app/locustio/confluence/locustfile.py
@@ -1,6 +1,30 @@
 from locust import HttpUser, task, between
+import logging
 
 from extension.confluence.extension_locust import app_specific_action
+# smartics uncomment for userscripts
+# from extension.confluence.extension_locust import app_specific_action_userscript_rest
+# from extension.confluence.extension_locust import app_specific_action
+
+# smartics comment in / out what you want to test. Must be synchron to the file(s) above
+# from extension.confluence.extension_locust import app_specific_action_docm
+
+# from extension.confluence.extension_locust import app_specific_action_docm_section
+# from extension.confluence.extension_locust import app_specific_action_docm_hide
+# from extension.confluence.extension_locust import app_specific_action_docm_hidefromreader
+# from extension.confluence.extension_locust import app_specific_action_docm_hidefromanonymous
+# from extension.confluence.extension_locust import app_specific_action_docm_definitionlist
+
+
+# smartics uncomment for projectdoc toolbox
+from extension.confluence.extension_locust import app_specific_action_transclude_documents
+from extension.confluence.extension_locust import app_specific_action_display_table
+
+# smartics uncomment for projectdoc toolbox extensions
+from extension.confluence.extension_locust import app_specific_action_web_api
+from extension.confluence.extension_locust import app_specific_action_information_system
+
+
 from locustio.common_utils import LocustConfig, MyBaseTaskSet
 from locustio.confluence.http_actions import login_and_view_dashboard, view_dashboard, view_blog, \
     search_cql_and_view_results, open_editor_and_create_blog, create_and_edit_page, comment_page, view_attachments, \
@@ -9,11 +33,11 @@ from util.conf import CONFLUENCE_SETTINGS
 
 config = LocustConfig(config_yml=CONFLUENCE_SETTINGS)
 
-
 class ConfluenceBehavior(MyBaseTaskSet):
 
     def on_start(self):
         self.client.verify = config.secure
+        logging.info("Here I am! SMARTICS")
         login_and_view_dashboard(self)
 
     @task(config.percentage('view_page'))
@@ -60,6 +84,52 @@ class ConfluenceBehavior(MyBaseTaskSet):
     def custom_action(self):
         app_specific_action(self)
 
+    @task(config.percentage('standalone_extension_transclude_documents'))
+    def custom_action_transclude_documents(self):
+        app_specific_action_transclude_documents(self)
+
+    @task(config.percentage('standalone_extension_display_table'))
+    def custom_action_display_table(self):
+        app_specific_action_display_table(self)
+
+    @task(config.percentage('standalone_extension_information_system'))
+    def custom_action_information_system(self):
+        app_specific_action_information_system(self)
+
+    # smartics uncomment for projectdoc toolbox extensions
+    @task(config.percentage('standalone_extension_web_api'))
+    def custom_action_web_api(self):
+        app_specific_action_web_api(self)
+
+'''
+# smartics uncomment for userscripts
+    @task(config.percentage('standalone_extension_us_rest_content'))
+    def custom_action_userscript_rest(self):
+        app_specific_action_userscript_rest(self)
+
+    @task(config.percentage('standalone_extension'))
+    def custom_action_is(self):
+        app_specific_action(self)
+'''
+
+'''
+    # smartics uncomment for documentationmacros
+    @task(config.percentage('standalone_extension_section'))
+    def custom_action_section(self):
+        app_specific_action_docm_section(self)
+
+    @task(config.percentage('standalone_extension_hide'))
+    def custom_action_hide(self):
+        app_specific_action_docm_hide(self)
+
+    @task(config.percentage('standalone_extension_hidefromreader'))
+    def custom_action_hidefromreader(self):
+        app_specific_action_docm_hidefromreader(self)
+
+    @task(config.percentage('standalone_extension_hidefromanonymous'))
+    def custom_action_hidefromanonymous(self):
+        app_specific_action_docm_hidefromanonymous(self)
+'''
 
 class ConfluenceUser(HttpUser):
     host = CONFLUENCE_SETTINGS.server_url
