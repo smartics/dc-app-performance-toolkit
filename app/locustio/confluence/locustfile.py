8cab655480764614691f20b03bbed16f1cca8eae
AntonKronseder <anton.kronseder@smartics.de>
2025-07-22 15:54:15 +0200

    allbackin

--- app/locustio/confluence/locustfile.py
+++ app/locustio/confluence/locustfile.py
@@ -64,6 +64,17 @@ from util.conf import CONFLUENCE_SETTINGS
 
 config = LocustConfig(config_yml=CONFLUENCE_SETTINGS)
 
+# --------------------------------------------------------------------------------------------
+# smartics-bp
+# --------------------------------------------------------------------------------------------
+
+'''
+logging.info("Here I am! SMARTICS I")
+with open('doctypes.txt', 'r') as file:
+    doctypes = [line.strip() for line in file if not line.strip().startswith("#")]
+doctypesALL = doctypes.copy()
+'''
+
 class ConfluenceBehavior(MyBaseTaskSet):
 
     def on_start(self):
