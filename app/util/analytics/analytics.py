f3c74a7379384fbe794bc48f0abfacad2045bc97
AntonKronseder <anton.kronseder@smartics.de>
2025-08-04 16:35:39 +0200

    undo

--- app/util/analytics/analytics.py
+++ app/util/analytics/analytics.py
@@ -74,10 +74,7 @@ class AnalyticsCollector:
             self.locust_log = LocustFileReader()
             self.post_run_collector = BambooPostRunCollector(self.locust_log)
         if self.app_type == CONFLUENCE:
-            try:
                 self.java_version = application.java_version
-            except Exception as e:
-                self.java_version = "N/A"
 
     def is_analytics_enabled(self):
         """
